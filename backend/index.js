b3c3948449a17ef0624bee14b106c810c38e1432
Ivan19811 <147108377+Ivan19811@users.noreply.github.com>
2025-06-21 14:24:30 +0300

    ♻️ Оновлено index.js — виправлення логіки proxy

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // ✅ Правильна CORS-конфігурація для Netlify-домену
 const corsOptions = {
-  origin: "https://crm.shifttime.com.ua",
+  origin: "https://shifttime-crm-test.onrender.com",
   methods: "GET,POST",
   allowedHeaders: ["Content-Type"]
 };
